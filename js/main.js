15215e0c58fed68d3b440d3e791835940b6b8282
joerr <jroche@hubspot.com>
2022-06-23 09:43:12 +0100

    Modal intro
    Semantic changes and modal intro on 2nd aside

--- js/main.js
+++ js/main.js
@@ -10,3 +10,16 @@ function sideMenuAppears() {
 function sideMenuDisappears() {
     document.getElementById('side-menu').classList.remove('show-menu');
 }
+
+document.getElementById('test-area').addEventListener('click', showModal);
+document.getElementById('close-dialog').addEventListener('click', closeModal);
+
+function showModal() {
+    document.getElementById('alert').classList.add('show-modal');
+    console.log("we get this far");
+}
+
+function closeModal() {
+    document.getElementById('alert').classList.remove('show-modal');
+    console.log("we get this far now");
+}
